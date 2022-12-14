c0bd7a57cafc7232f4f25778fd7e98f2e47a8e0a
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-14 14:08:07 -0800

    downgraded to v13.0.4 nextjs for hot reloading to work

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Image from "next/image"
 // download headwind extension to autosort classnames so we see the same thing
 export default function Home() {
   return (
-    <div className="font-mono font-bold">
+    <div className="">
       <div></div>
-      <button className="text-white bg-slate-500">Click Me</button>
+      <button className="px-3 py-2 m-3 font-sans text-white bg-blue-500 rounded-2xl font-inter">
+        Click Me
+      </button>
     </div>
   )
 }
