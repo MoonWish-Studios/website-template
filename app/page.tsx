d0569c0e22803c6b856e8712ff35d5d0d8ddf0f8
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-12-22 10:32:44 -0800

    added to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Image from "next/image"
-
+"use client"
 // download headwind extension to autosort classnames so we see the same thing
 export default function Home() {
-  return (
-    <div className="">
-      <div></div>
-      <button className="px-3 py-2 m-3 font-sans text-white bg-blue-500 rounded-2xl font-inter">
-        Click Me
-      </button>
-    </div>
-  )
+  return <div>Home</div>
 }
